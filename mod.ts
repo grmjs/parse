bbac5cac9bc85e0e4ef91bb518e2113d39a476cc
Roj <ez@roj.im>
2023-03-17 16:49:10 +0300

    chore: bump grm

--- mod.ts
+++ mod.ts
@@ -1,8 +1,8 @@
-import { Api } from "https://deno.land/x/grm@0.5.4/mod.ts";
+import { Api } from "https://deno.land/x/grm@0.8.2/mod.ts";
 import {
   EditMessageParams,
   SendMessageParams,
-} from "https://deno.land/x/grm@0.5.4/src/client/types.ts";
+} from "https://deno.land/x/grm@0.8.2/src/client/types.ts";
 
 const entities = {
   "MessageEntityBold": Api.MessageEntityBold,
